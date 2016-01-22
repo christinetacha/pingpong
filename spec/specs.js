4766bb3101b13a66bb79144db1dfe52b67beb61e
Christine Tacha <tacha.christine@gmail.com>
2016-01-22 12:37:08 -0800

    add the spec to divide by 5

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
 describe ("divisibleByThree", function () {
-  it("it is divisible by three", function() {
+  it("will determine if the number divisible by three", function() {
     expect(divisibleByThree(9)).to.equal(true);
+  });
+  it("will determine if the number is divisible by five", function() {
+    expect(divisibleByFive(15)).to.equal(true);
   });
 });
