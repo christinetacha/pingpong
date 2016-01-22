58420a9646205d521bac593a641f5629cf314406
Christine Tacha <tacha.christine@gmail.com>
2016-01-22 14:20:36 -0800

    add js to pass specs for returning ping, pong, pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,11 @@
 var divisibleBy = function(number) {
-  if (number % 3 === 0) {
-    return true;
+  if (number % 3 === 0 && number % 5 !== 0) {
+    return "ping";
+  } else if (number % 5 === 0 && number % 3 !== 0) {
+    return "pong";
+  } else if (number % 3 === 0 && number % 5 === 0) {
+    return "pingpong";
   } else {
-    return false;
-  }
-  if (number % 5 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-  if (number % 3 === 0 && number % 5 === 0) {
-    return true;
-  } else {
-    return false;
+    return number;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,11 @@
 describe ("divisibleBy", function () {
-  it("will determine if the number divisible by three", function() {
-    expect(divisibleBy(9)).to.equal(true);
+  it("will determine if the number divisible by three and should ping", function() {
+    expect(divisibleBy(9)).to.equal("ping");
   });
-  it("will determine if the number is divisible by five", function() {
-    expect(divisibleBy(15)).to.equal(true);
+  it("will determine if the number is divisible by five and should pong", function() {
+    expect(divisibleBy(25)).to.equal("pong");
   });
-  it("will determine if the number is divisible by three and five", function() {
-    expect(divisibleBy(30)).to.equal(true);
+  it("will determine if the number is divisible by three and five and should pingpong", function() {
+    expect(divisibleBy(30)).to.equal("pingpong");
   });
 });
