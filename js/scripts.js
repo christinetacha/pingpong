d5dbf6d24fcf997c215ecbfac026053d1d66374e
Christine Tacha <tacha.christine@gmail.com>
2016-01-27 19:58:11 -0800

    all specs passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 var divisibleBy = function(number) {
-  if (number % 3 === 0 && number % 5 !== 0) {
-    return "ping";
-  } else if (number % 5 === 0 && number % 3 !== 0) {
-    return "pong";
-  } else if (number % 3 === 0 && number % 5 === 0) {
+  if (number % 3 === 0 && number % 5 == 0) {
     return "pingpong";
+  } else if (number % 5 === 0) {
+    return "pong";
+  } else if (number % 3 === 0) {
+    return "ping";
   } else {
     return number;
   }
 };
 
 var looptyLoo = function(number) {
-  var arrayOne = [];
+  var divisibleResult = [];
   for (var i = 1; i <= number; i++) {
-    arrayOne.push(divisibleBy(i));
+    divisibleResult.push(divisibleBy(i));
   }
-    return arrayOne;
+    return divisibleResult;
 };
+
+$(document).ready(function() {
+    $("form#pingPong").submit(function(event) {
+      $(".result").show();
+    });
+  });
